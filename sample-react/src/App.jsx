4887e5e85eecbd38d79bdf96404a8a859454214c
nivash77 <nivashmareesh07@gmail.com>
2024-09-16 22:23:27 +0530

    add-style.css-login-components

--- sample-react/src/App.jsx
+++ sample-react/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from "./components/login"
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const App=()=>{
+  const nav="Sign-Up"
+    return (
+        <>
+        <div className="h-screen w-screen flex  flex-col justify-center items-center bg-zinc-300">
+         
+          <div className="h-4/6 w-2/6  bg-gray-500  rounded-md flex flex-col justify-center items-center gap-4">
+          <Login login={nav} />
+         
+          <input type="text" placeholder="NAME"  required className="h-8 w-3/6  hover:border-b-4 hover:border-b-gray-700 rounded-sm"/>
+          <input type="number" placeholder="AGE" className="h-8 w-3/6 hover:border-b-4 hover:border-b-gray-700 rounded-sm"/>
+          <input type="email" placeholder="Email" className="h-8 w-3/6 hover:border-b-4  hover:border-b-gray-700 rounded-sm "/>
+          <input type="password" placeholder="Password" className="h-8 w-3/6 hover:border-b-4  hover:border-b-gray-700 rounded-sm "/>
+           <button className= " w-1/5 block bg-gray-700  text-white p-2 rounded-md hover:bg-gray-600" >Sign-up </button>
+          </div>
+        </div>
+        </>
+      )
+    
 }
-
 export default App
---
+++ sample-react/src/components/login.jsx
@@ -0,0 +1,10 @@
+const Login=({login})=>{
+    return(
+        <>
+        <div className="pb-5">
+        <img src="src\assets\img\8029531c433b7d0e2bf48ca6429ef325.jpg" className="size-9 rounded-full ml-7"></img>
+        <h1 className="font-bold text-3xl">{login}</h1></div>
+        </>
+    )
+}
+export default Login;
