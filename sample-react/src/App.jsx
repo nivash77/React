0f3fe5a715a770f6e1950f29334acd1966e8a133
nivash77 <nivashmareesh07@gmail.com>
2024-09-16 22:43:50 +0530

    add-array-map-filter

--- sample-react/src/App.jsx
+++ sample-react/src/App.jsx
@@ -5,12 +5,13 @@ import Login from "./components/login"
 
 const App=()=>{
   const nav="Sign-Up"
+  const arr=['react','angular','js','ts','html','css']
     return (
         <>
         <div className="h-screen w-screen flex  flex-col justify-center items-center bg-zinc-300">
          
           <div className="h-4/6 w-2/6  bg-gray-500  rounded-md flex flex-col justify-center items-center gap-4">
-          <Login login={nav} />
+          <Login login={nav} arry={arr} />
          
           <input type="text" placeholder="NAME"  required className="h-8 w-3/6  hover:border-b-4 hover:border-b-gray-700 rounded-sm"/>
           <input type="number" placeholder="AGE" className="h-8 w-3/6 hover:border-b-4 hover:border-b-gray-700 rounded-sm"/>
